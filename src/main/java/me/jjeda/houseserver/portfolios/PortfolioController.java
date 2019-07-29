a0b700ccdb221f158b1d3335451ebf55e2511f85
jjeda <jjeda@naver.com>
2019-07-29 12:34:29 +0900

    GET portfolios Test code

--- src/main/java/me/jjeda/houseserver/portfolios/PortfolioController.java
+++ src/main/java/me/jjeda/houseserver/portfolios/PortfolioController.java
@@ -2,21 +2,24 @@ package me.jjeda.houseserver.portfolios;
 
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Optional;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping(value ="/api/portfolios" , produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+@RequestMapping(value = "/api/portfolios", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class PortfolioController {
 
     private final PortfolioRepository portfolioRepository;
@@ -26,7 +29,7 @@ public class PortfolioController {
     @PostMapping
     public ResponseEntity createPortfolio(@RequestBody Portfolio requestPortfolio) {
 
-        Portfolio portfolio =modelMapper.map(requestPortfolio, Portfolio.class);
+        Portfolio portfolio = modelMapper.map(requestPortfolio, Portfolio.class);
 
         Portfolio newPortfolio = this.portfolioRepository.save(portfolio);
         ControllerLinkBuilder selfLinkBuilder = linkTo(PortfolioController.class).slash(newPortfolio.getId());
@@ -36,5 +39,27 @@ public class PortfolioController {
         return ResponseEntity.created(createdUri).body(portfolioResource);
     }
 
+    @GetMapping
+    public ResponseEntity queryPortfolios(Pageable pageable,
+                                          PagedResourcesAssembler<Portfolio> assembler) {
+        Page<Portfolio> page = this.portfolioRepository.findAll(pageable);
+        PagedResources pagedResources = assembler.toResource(page, e -> new PortfolioResource(e));
+
+        return ResponseEntity.ok(pagedResources);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getPortfolios(@PathVariable Integer id) {
+
+        Optional<Portfolio> optionalPortfolio = this.portfolioRepository.findById(id);
+
+        if (optionalPortfolio.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Portfolio portfolio = optionalPortfolio.get();
+        PortfolioResource portfolioResource = new PortfolioResource(portfolio);
+        return ResponseEntity.ok(portfolioResource);
+    }
 
 }
--- src/test/java/me/jjeda/houseserver/portfolios/PortfolioControllerTest.java
+++ src/test/java/me/jjeda/houseserver/portfolios/PortfolioControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -44,8 +46,60 @@ public class PortfolioControllerTest extends BaseControllerTest {
 
     }
 
+    @Test
+    @TestDescription("30개의 포트폴리오를 10개씩 2번째 페이지 조회하기")
+    public void queryPortfolios() throws Exception {
+        //Given
+        IntStream.range(0,30).forEach(this::generatePortfolio);
+
+        //When & Then
+        this.mockMvc.perform(get("/api/portfolios")
+                .param("page","1")
+                .param("size","10")
+                .param("sort","id,DESC"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("page").exists())
+                .andExpect(jsonPath("_embedded.portfolioList[0]._links.self").exists())
+                .andExpect(jsonPath("_links.self").exists());
+    }
 
 
+    @Test
+    @TestDescription("기존의 포트폴리오를 하나 조회하기")
+    public void getPortfolio() throws Exception {
+
+        Portfolio portfolio = this.generatePortfolio(100);
+
+        this.mockMvc.perform(get("/api/portfolios/{id}",portfolio.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("title").exists())
+                .andExpect(jsonPath("contents").exists())
+                .andExpect(jsonPath("_links.self").exists());
 
 
+    }
+
+    @Test
+    @TestDescription("없는 포트폴리오를 조회했을 때 404 응답받기")
+    public void getPortfolio404() throws Exception {
+        this.mockMvc.perform(get("/api/portfolio/1234123"))
+                .andExpect(status().isNotFound());
+
+    }
+
+    private Portfolio generatePortfolio(int index) {
+        Portfolio portfolio = buildPortfolio(index);
+        return this.portfolioRepository.save(portfolio);
+    }
+
+    private Portfolio buildPortfolio(int index) {
+        return Portfolio.builder()
+                .title("Portfolio Test"+ index)
+                .contents("Rest API Development with Spring")
+                .createdDateTime(LocalDateTime.now())
+                .build();
+    }
 }
